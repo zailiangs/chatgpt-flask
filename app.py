44469b80156f85a5d0ec224c45fcd950e9efbf44
Zailiang <sunzailiang@icloud.com>
2023-05-08 16:58:03 +0800

    111

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}})
 app.register_blueprint(chat_bp)
 # 配置数据库连接
 connect = pymysql.connect(host="sh-cdb-mns9cip2.sql.tencentcdb.com", port=63682, user="root", password="Passgz6374",
-                          db="yk_rank", charset="utf8", autocommit=True)
+                          db="yk_rank", charset="utf8", autocommit=True, connect_timeout=10, read_timeout=10)
 
 
 def execute_sql(sql, args=None):
